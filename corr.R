aa8fd142b8743d9ee2107bbd48d367fbfe15cf62
anthonywittemann <awittema@usc.edu>
2015-07-16 15:55:35 -0700

    can find the correlation between two data files

--- corr.R
+++ corr.R
@@ -9,4 +9,28 @@ corr <- function(directory, threshold = 0) {
     
     ## Return a numeric vector of correlations
     ## NOTE: Do not round the result!
+    
+    # set working directory
+    if(grep("specdata", directory) == 1) {
+        directory <- ("./specdata/")
+    }
+    # get the complete table
+    completeTable <- complete("specdata", 1:332)
+    nobs <- completeTable$nobs
+    # find the valid ids that are > threshhold
+    ids <- completeTable$id[nobs > threshold]
+    # get the length of ids vector
+    idLen <- length(ids)
+    corrVec <- rep(0, idLen)
+    # find all files in the specdata folder
+    allFiles <- as.character( list.files(directory) )
+    filePaths <- paste(directory, allFiles, sep="")
+    j <- 1
+    for(i in ids) {
+        currentFile <- read.csv(filePaths[i], header=T, sep=",")
+        corrVec[j] <- cor(currentFile$sulfate, currentFile$nitrate, use="complete.obs")
+        j <- j + 1
+    }
+    result <- corrVec
+    return(result)   
 }
