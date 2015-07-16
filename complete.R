1d26e28e8ff5e05b4201f5866c405dae309944fa
anthonywittemann <awittema@usc.edu>
2015-07-16 15:05:58 -0700

    completed parts 1,2 of programming assignment 1

--- complete.R
+++ complete.R
@@ -12,4 +12,23 @@ complete <- function(directory, id = 1:332) {
     ## ...
     ## where 'id' is the monitor ID number and 'nobs' is the
     ## number of complete cases
+    
+    # set working directory
+    if(grep("specdata", directory) == 1) {
+        directory <- ("./specdata/")
+    }
+    # get the length of id vector
+    idLen <- length(id)
+    completeData <- rep(0, idLen)
+    # find all files in the specdata folder
+    allFiles <- as.character( list.files(directory) )
+    filePaths <- paste(directory, allFiles, sep="")
+    j <- 1 
+    for (i in id) {
+        currentFile <- read.csv(filePaths[i], header=T, sep=",")
+        completeData[j] <- sum(complete.cases(currentFile))
+        j <- j + 1
+    }
+    result <- data.frame(id = id, nobs = completeData)
+    return(result)
 }
--- pollutantmean.R
+++ pollutantmean.R
@@ -13,5 +13,22 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
     ## in the 'id' vector (ignoring NA values)
     ## NOTE: Do not round the result!
   
-  
+    # set working directory
+    if(grep("specdata", directory) == 1) {
+        directory <- ("./specdata/")
+    }
+    # vector for pollutant data
+    meanVec <- c()
+    # find all files in the specdata folder
+    allFiles <- as.character( list.files(directory) )
+    filePaths <- paste(directory, allFiles, sep="")
+    for(i in id) {
+        currentFile <- read.csv(filePaths[i], header=T, sep=",")
+        head(currentFile)
+        pollutant
+        naRemoved <- currentFile[!is.na(currentFile[, pollutant]), pollutant]
+        meanVec <- c(meanVec, naRemoved)
+    }
+    result <- mean(meanVec) # DO NOT ROUND THE RESULT
+    return(result) 
 }
